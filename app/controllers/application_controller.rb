bdf004881ef39c499a9ceede5fad81522b1d8023
Frankie Roberto <frankie@frankieroberto.com>
2012-11-19 12:23:09 +0000

    Removing this

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
 
-  rescue_from(ActiveRecord::RecordNotFound) {
-    respond_to do |format|
-      format.html { render :template => "errors/not_found.html", :status => 404, :layout => "application" }
-    end
-  }
+#  rescue_from(ActiveRecord::RecordNotFound) {
+#    respond_to do |format|
+#      format.html { render :template => "errors/not_found.html", :status => 404, :layout => "application" }
+#    end
+#  }
 
 end
