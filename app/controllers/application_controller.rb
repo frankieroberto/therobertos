2defead4a03e6457cee35cdb03fbe090050ab5b7
Frankie Roberto <frankie@frankieroberto.com>
2012-11-19 12:36:09 +0000

    Redeclaring a NotFoundError class

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
+class NotFoundError < StandardError; end
+
 class ApplicationController < ActionController::Base
 
-#  rescue_from(ActiveRecord::RecordNotFound) {
-#    respond_to do |format|
-#      format.html { render :template => "errors/not_found.html", :status => 404, :layout => "application" }
-#    end
-#  }
+  rescue_from(StandardError::NotFoundError) {
+    respond_to do |format|
+      format.html { render :template => "errors/not_found.html", :status => 404, :layout => "application" }
+    end
+  }
 
 end
--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,7 +1,7 @@
 class ErrorsController < ApplicationController
 
   def not_found
-    raise ActiveRecord::RecordNotFound
+    raise NotFoundError
   end
 
 end
