21ae903f10a512ee68157561cf99fbe8095602eb
Frankie Roberto <frankie@frankieroberto.com>
2013-09-08 22:52:50 +0100

    work-in-progress

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,17 @@ Therobertos::Application.routes.draw do
   end
 
   resource :bigcamp, :controller => :big_camp, :only => :show, :id => 'introduction' do
+    resource :northern_spain, :controller => :big_camp, :id => 'northern_spain', :only => :show
+    resource :portugal, :controller => :big_camp, :id => 'portugal', :only => :show
+    resource :andalucia, :controller => :big_camp, :id => 'andalucia', :only => :show
+    resource :aragon, :controller => :big_camp, :id => 'aragon', :only => :show
+    resource :catalonia, :controller => :big_camp, :id => 'catalonia', :only => :show
+    resource :provence, :controller => :big_camp, :id => 'provence', :only => :show
+    resource :ardeche, :controller => :big_camp, :id => 'ardeche', :only => :show
+    resource :cevennes, :controller => :big_camp, :id => 'cevennes', :only => :show
+    resource :alps, :controller => :big_camp, :id => 'alps', :only => :show
+    resource :paris, :controller => :big_camp, :id => 'paris', :only => :show
+
   end
 
 	match "/europe2013" => redirect("/bigcamp")
